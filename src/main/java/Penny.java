83559fa4194ba66085a5614f1e49b8b47bcd58c4
Jimi <238704011+JimiBong@users.noreply.github.com>
2026-08-18 12:58:17 +0800

    Renamed to Penny, added greet and exit

--- src/main/java/Penny.java
+++ src/main/java/Penny.java
@@ -1,10 +1,12 @@
-public class Duke {
+public class Penny {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        banner += "\nHi, I'm Penny, what can I do for you?";
+        banner += "\nBye! See you soon!";
         System.out.println(banner);
     }
 }
